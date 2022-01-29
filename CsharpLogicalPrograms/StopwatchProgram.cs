38ed21750a7421b3faa6ae945f567b14d20587dc
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-01-29 13:14:32 +0530

    [Raj] Modified:- The stopwatch program

--- CsharpLogicalPrograms/StopwatchProgram.cs
+++ CsharpLogicalPrograms/StopwatchProgram.cs
@@ -15,7 +15,7 @@ namespace CsharpLogicalPrograms
             int ch;
             do
             {
-                Console.WriteLine("1. START\n2. STOP \n3. Elapsed \n4. EXIT");
+                Console.WriteLine("1. Start\n2. Stop \n3. Elapsed \n4. Exit");
                 Console.Write("Enter Your Choice : ");
                 ch = int.Parse(Console.ReadLine());
                 switch (ch)
@@ -27,7 +27,7 @@ namespace CsharpLogicalPrograms
                         watch.Stop();
                         break;
                     case 3:
-                        double elapsedTime = watch.ElapsedMilliseconds/1000;
+                        double elapsedTime = Math.Round((double)watch.ElapsedMilliseconds/1000,2);
                         Console.WriteLine("Elapsed Time is : "+elapsedTime+ " Seconds");
                         Console.ReadLine(); 
                         break;
